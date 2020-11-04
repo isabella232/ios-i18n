1896ad08732c21ed0abaab28a53ee96cf05fb5c4
Damian Carrillo <dcarrillo@atlassian.com>
2020-11-04 16:08:01 -0600

    Add carthage support

--- Smartling.i18n/SmartlingLib.h
+++ Smartling.i18n/SmartlingLib.h
@@ -18,6 +18,8 @@
 //  Created by Pavel Ivashkov on 2015-05-15.
 //
 
+#import <Foundation/Foundation.h>
+
 //! Project version number for SmartlingLib.
 FOUNDATION_EXPORT double SmartlingLibVersionNumber;
 
@@ -26,5 +28,4 @@ FOUNDATION_EXPORT const unsigned char SmartlingLibVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SmartlingLib/PublicHeader.h>
 
-
-#import "SLLocalization.h"
+#import <SmartlingLib/SLLocalization.h>
